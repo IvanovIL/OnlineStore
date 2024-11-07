5c5aec19b44f3e651264c6fa4f7da3d2cfe930fe
Stefan Flyagin <ila723483@gmail.com>
2024-11-07 15:04:33 +0300

    j

---
+++ Contracts/OnlineStore.Contracts/Enum/ProductAttributeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace OnlineStore.Contracts.Enum
+{
+	public enum ProductAttributeEnum
+	{
+		[Description("Используется для поиска")]
+		ForSearch = 1,
+
+		[Description("Не используется для поиска")]
+		NotForSearch = 2
+	}
+}
---
+++ Infrastructure/OnlineStore.DataAccess/Configuration/ProductAttributeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.DataAccess.Configuration
+{
+	public sealed class ProductAttributeConfiguration : IEntityTypeConfiguration<ProductAttribute>
+	{
+		public void Configure(EntityTypeBuilder<ProductAttribute> builder)
+		{
+			builder.ToTable("Attributes");
+			builder.HasKey(e => e.Id);
+
+			builder.Property(t => t.Id).
+				HasColumnName("Id");
+
+			builder.Property(t => t.Name).
+				HasColumnName("Name").IsRequired(true);
+
+			builder.Property(t => t.Description).
+				HasColumnName("Description");
+		}
+	}
+}
